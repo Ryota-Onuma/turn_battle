015e262386d706a18179ab00991335fc30e123e0
Ryota <onumaryotajh@gmail.com>
2021-05-09 11:07:12 +0900

    rubocop -a かけた

--- rails/config/puma.rb
+++ rails/config/puma.rb
@@ -4,7 +4,8 @@ min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 
 threads min_threads_count, max_threads_count
 
-# port        ENV.fetch("PORT") { 3000 } 
+# port        ENV.fetch("PORT") { 3000 }をコメントアウトすると、nginx経由でしかrailsにアクセスできなくなる↓
+# port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch('RAILS_ENV') { 'development' }
 
 plugin :tmp_restart
